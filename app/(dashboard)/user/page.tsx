eb6a83eaba2de0e823830e4c06adad56da20fd06
alongkot <alongkotwimonsuk@gmail.com>
2025-09-25 12:44:02 +0700

    feat(ui): add users and transaction placeholder pages

---
+++ app/(dashboard)/user/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react';
+
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Welcome to the Tutorium User</h1>
+        </div>
+    );
+}
